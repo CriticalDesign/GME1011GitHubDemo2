eced9966fc4bb115630a4c65cb5d371b3c78c125
Aaron Langille <aaron.langille@gmail.com>
2024-06-20 10:14:57 -0400

    Changed back to CFBlue.

--- GME1011GitHubDemo2/Game1.cs
+++ GME1011GitHubDemo2/Game1.cs
@@ -42,7 +42,7 @@ namespace GME1011GitHubDemo2
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Firebrick);
+            GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
 
